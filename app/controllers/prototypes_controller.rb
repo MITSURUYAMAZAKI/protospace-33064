e837d0a004c93c0f549d4e13a7e95f18c5ad4b59
MITSURUYAMAZAKI <75934231+MITSURUYAMAZAKI@users.noreply.github.com>
2021-01-13 19:06:30 +0900

    レビュー後修正１

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,8 @@
 class PrototypesController < ApplicationController
-  # before_action :set_prototype, except: [:index, :new, :create]
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -21,20 +22,17 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to root_path
-    end
+    # @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -43,7 +41,7 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to root_path
     else
@@ -53,7 +51,16 @@ class PrototypesController < ApplicationController
 
   private
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
 end
+
